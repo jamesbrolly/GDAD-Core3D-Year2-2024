143ae0e9ec84e7c15cdef706c4d923ddfd9eca1e
jamesbrolly <148452080+jamesbrolly@users.noreply.github.com>
2024-10-01 16:00:00 +0100

    health potion

--- Assets/CRE340/Game2-ItemsAndClasses/Scripts/HealthPotion.cs
+++ Assets/CRE340/Game2-ItemsAndClasses/Scripts/HealthPotion.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class HealthPotion : Item        //Derived class HealthPotion that inherits from Item
 {
-    // Start is called before the first frame update
-    void Start()
+    public int healthRestoreAmount;     // Amount of health this potion restores
+    public int minRestoreAmount = 30;   // Minumum restore amount for random range
+    public int maxRestoreamount = 70;   // Maximum restore amount for random range
+
+    // Defauly Constructor, sets the name and description of the health potion
+
+    public HealthPotion()
     {
-        
+        itemName = "Health Potion";
+        description = "A potion that restores health.";
     }
 
-    // Update is called once per frame
-    void Update()
+    // Called when the object is instantiated
+    private void Start()
     {
-        
+        // Assign a random value for healthRestoreAmount within the specified range
+        healthRestoreAmount = Random.Range(minRestoreAmount, maxRestoreamount);
+        Debug.Log($"HealthPotion: Random restore amount set to {healthRestoreAmount}.");
     }
+
+    // Override metof ti display specific health potion info
+    
+
+
 }
